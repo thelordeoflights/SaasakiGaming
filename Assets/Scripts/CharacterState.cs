e9c8da7040c37df617879d71f4d7a9c73a91be64
thelordeoflights <70798663+thelordeoflights@users.noreply.github.com>
2024-06-04 09:13:46 +0530

    stateHandler
    stateHandler

--- Assets/Scripts/CharacterState.cs
+++ Assets/Scripts/CharacterState.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CharacterState", menuName = "CharacterState", order = 0)]
 public class CharacterState : ScriptableObject
 {
-    public enum States { Walking, Idle, Jumping, Sprint };
+    public enum States { Idle, Jumping, Sprint };
     public States activeStates = States.Idle;
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
     [SerializeField] PlayerInput playerInput;
     [SerializeField] CharacterState characterState;
+    [SerializeField] Animator animator;
+
+    [SerializeField] float jumpSpeed;
+    float ySpeed;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    Vector2 input;
 
     void Update()
     {
-        CheckStates();
-        Movement();
+        HandleInput();
     }
-    void CheckStates()
+    void HandleInput()
     {
+        if (playerInput.actions["Move"].triggered || playerInput.actions["Move"].IsPressed() && characterState.activeStates != CharacterState.States.Jumping)
+        {
+            characterState.activeStates = CharacterState.States.Sprint;
+        }
+        if (playerInput.actions["Jump"].triggered)
+        {
+            characterState.activeStates = CharacterState.States.Jumping;
+        }
+        else
+        {
+            characterState.activeStates = CharacterState.States.Idle;
+        }
+        CheckState();
+    }
+    void CheckState()
+    {
+        switch (characterState.activeStates)
+        {
+            case CharacterState.States.Sprint:
+                input = playerInput.actions["Move"].ReadValue<Vector2Int>();
+                animator.SetTrigger("_Run");
+                animator.ResetTrigger("_Idle");
+                animator.ResetTrigger("_Jump");
+                Movement();
+                break;
+            case CharacterState.States.Idle:
+                input = Vector2.zero;
+                animator.SetTrigger("_Idle");
+                animator.ResetTrigger("_Run");
+                animator.ResetTrigger("_Jump");
+                break;
+            case CharacterState.States.Jumping:
+                Jump();
+                animator.SetTrigger("_Jump");
+                animator.ResetTrigger("_Run");
+                animator.ResetTrigger("_Idle");
+                break;
+
+        }
+    }
+    public void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            ySpeed = -0.5f;
+            ySpeed = jumpSpeed;
+            characterState.activeStates = CharacterState.States.Jumping;
+        }
 
     }
 
@@ -32,7 +85,6 @@ public class PlayerMovement : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
         controller.Move(move * Time.deltaTime * playerSpeed);
 
